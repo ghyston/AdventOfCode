000d3fdd9c745f6d86a8fa210d5b3a4085206180
Ilja Stepanow <ghyston@gmail.com>
2020-12-17 18:41:56 +0100

    Day11

--- Program.cs
+++ Program.cs
@@ -1,25 +1,22 @@
 ﻿using System;
-using System.Linq;
 
-Console.WriteLine($"Day1: {Day1.SumTo2020()}");
-Console.WriteLine($"Day2: {Day2.RightPasswordsCount()}");
-Console.WriteLine($"Day3 part 1: {Day3.TreesOnSlopeCount(3,1)}");
-Console.WriteLine($"Day3 part 2: {Day3.TreesOnAllSlopesCount()}");
-Console.WriteLine($"Day4 part 1: {Day4.ValidPassports()}");
-Console.WriteLine($"Day5 part 1: {Day5.HighestID()}");
-Console.WriteLine($"Day5 part 2: {Day5.MissingId()}");
-Console.WriteLine($"Day6 part 1: {Day6.GetAnyoneSum()}");
-Console.WriteLine($"Day6 part 2: {Day6.GetEveryoneSum()}");
-Console.WriteLine($"Day7 part 1: {Day7.CanContainShinyBagCount()}");
-Console.WriteLine($"Day7 part 2: {Day7.ShinyBagCanContain()}");
-Console.WriteLine($"Day8 part 1: {Day8.ValueBeforeInf()}");
-Console.WriteLine($"Day8 part 2: {Day8.BruteforceResult()}");
-Console.WriteLine($"Day9 part 1: {Day9.FindIncorrect()}");
-Console.WriteLine($"Day9 part 2: {Day9.FindIncorrectSum()}");
-Console.WriteLine($"Day10 part 1: {Day10.MultiplyJoints()}");
-Console.WriteLine($"Day10 part 2: {Day10.CalculateAllPossibleJoints()}");
+Console.WriteLine($"No main, no gain");
 
-
-
-
-
+//Console.WriteLine($"Day1: {Day1.SumTo2020()}");
+//Console.WriteLine($"Day2: {Day2.RightPasswordsCount()}");
+//Console.WriteLine($"Day3 part 1: {Day3.TreesOnSlopeCount(3,1)}");
+//Console.WriteLine($"Day3 part 2: {Day3.TreesOnAllSlopesCount()}");
+//Console.WriteLine($"Day4 part 1: {Day4.ValidPassports()}");
+//Console.WriteLine($"Day5 part 1: {Day5.HighestID()}");
+//Console.WriteLine($"Day5 part 2: {Day5.MissingId()}");
+//Console.WriteLine($"Day6 part 1: {Day6.GetAnyoneSum()}");
+//Console.WriteLine($"Day6 part 2: {Day6.GetEveryoneSum()}");
+//Console.WriteLine($"Day7 part 1: {Day7.CanContainShinyBagCount()}");
+//Console.WriteLine($"Day7 part 2: {Day7.ShinyBagCanContain()}");
+//Console.WriteLine($"Day8 part 1: {Day8.ValueBeforeInf()}");
+//Console.WriteLine($"Day8 part 2: {Day8.BruteforceResult()}");
+//Console.WriteLine($"Day9 part 1: {Day9.FindIncorrect()}");
+//Console.WriteLine($"Day9 part 2: {Day9.FindIncorrectSum()}");
+//Console.WriteLine($"Day10 part 1: {Day10.MultiplyJoints()}");
+//Console.WriteLine($"Day10 part 2: {Day10.CalculateAllPossibleJoints()}");
+Console.WriteLine($"Day11 part 2: {Day11.CalculateIterations()}");
