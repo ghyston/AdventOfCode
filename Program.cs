7505637a26d016a1c7b72386da902c13408ddd9e
Ilja Stepanow <ghyston@gmail.com>
2021-01-06 11:14:08 +0100

    Day 14 part 1

--- Program.cs
+++ Program.cs
@@ -22,4 +22,5 @@ Console.WriteLine($"No main, no gain");
 //Console.WriteLine($"Day11 part 2: {Day11.CalculateIterations()}");
 //Console.WriteLine($"Day12 part 1: {Day12.WhereAreWe()}");
 //Console.WriteLine($"Day12 part 2: {Day12.WhereAreWeReally()}");
-Console.WriteLine($"Day13 part 1: {Day13.Part2()}");
+//Console.WriteLine($"Day13 part 1: {Day13.Part2()}");
+Console.WriteLine($"Day14 part 1: {Day14.Part1()}");
